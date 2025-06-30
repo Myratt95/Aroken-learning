120756e7396f98b5df4744ba6779796d43117ae8
Myratt95 <163718861+Myratt95@users.noreply.github.com>
2025-06-30 15:06:23 +0300

    Add files via upload

--- homework-11/js/main.js
+++ homework-11/js/main.js
@@ -15,6 +15,70 @@
         }  else document.body.classList.remove('body--opened-modal')
     }
 
+    // Табы
+
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+
+        e.preventDefault ()
+
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+        e.preventDefault ()
+
+        const tabContentID = tabControl.getAttribute('href')
+
+        document.querySelector('.tab-controls__link--active').classList.remove('tab-controls__link--active')
+        tabControl.classList.add('tab-controls__link--active')
+        document.querySelector('.tab-content--show').classList.remove('tab-content--show')
+        document.querySelector(tabContentID).classList.add('tab-content--show')
+        
+    }
+
+    // Аккордеон
+
+    const accordionLists = document.querySelectorAll('.accordion-list')
+    
+
+    
+    accordionLists.forEach(el =>  {
+
+        el.addEventListener('click', (e) => {
+        
+        const accordionList = e.currentTarget
+        const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+        const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+        
+        const accordionControl = e.target.closest('.accordion-list__control');
+        if (!accordionControl) return
+        const accordionItem = accordionControl.parentElement;
+        const accordionContent = accordionControl.nextElementSibling;
+        
+        if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+            accordionOpenedItem.classList.remove('accordion-list__item--opened')
+            accordionOpenedContent.style.maxHeight = null
+        }
+
+        accordionItem.classList.toggle('accordion-list__item--opened')
+
+        if (accordionItem.classList.contains('accordion-list__item--opened')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        } else {
+            accordionContent.style.maxHeight = null
+        }
+
+    });
+
+    });
+
+
 })()
 
 const button = document.querySelector('.burger-icon')
